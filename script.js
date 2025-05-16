921f53a11e8a76861deeff3471132d9898629add
Sundari <sundarinnagraj@gmail.com>
2025-05-16 16:01:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var a=16;
+console.log(a)
+var b=20;
+console.log(b)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a<b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a==b)
+function add(){
+    var a=10;
+    var b=23;
+    console.log(a+b)
+}
+add()
+function add(a,b){
+    console.log(a+b)
+}
+add(10,30)
+add(40,70)
+add(20,13)
+sub()
+function sub(a,b){
+    console.log(a-b)
+}
+sub(12,21)
